824bb8f0232bd65d1aa6249c70f2ac01f73cc1bc
MechaEdgar <ee1206@hotmail.com>
2016-02-26 13:30:46 -0600

    A little story teller web app

---
+++ scripts.js
@@ -0,0 +1,9 @@
+var bla = "there was a time a ";
+var noun = prompt("Hello there, give me a noun");
+bla +=  noun;
+console.log(noun);
+console.log(bla);
+var verb = prompt("Hello again, give me a verb please");
+console.log(verb);
+bla += " that really enjoys to " + verb;
+document.write(bla);
