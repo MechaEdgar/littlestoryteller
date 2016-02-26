6e3ef4676499279d5510d3a080c593107257e37c
MechaEdgar <ee1206@hotmail.com>
2016-02-26 13:57:05 -0600

    Adding h2 tag to var tale.

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
-var tale = "there was a time a ";
+var tale = "<h2> there was a time a ";
 var noun = prompt("Hello there, give me a noun");
 tale +=  noun;
 console.log(noun);
 console.log(tale);
 var verb = prompt("Hello again, give me a verb please");
 console.log(verb);
-tale += " that really enjoys to " + verb;
+tale += " that really enjoys to " + verb + ".</h2>";
 document.write(tale);
