e886b9938b2153b5c92f09d82cbf897760d0e866
MechaEdgar <ee1206@hotmail.com>
2016-02-27 11:56:52 -0600

    Questions left added

--- scripts.js
+++ scripts.js
@@ -1,9 +1,13 @@
+var questions = 2;
+var questionsLeft = ' [' + questions + ' questions left]';
 var tale = "<h2> there was a time a ";
-var noun = prompt("Hello there, give me a noun");
+var noun = prompt("Hello there, give me a noun " + questionsLeft);
+questions -= 1;
+var questionsLeft = ' [' + questions + ' questions left]';
 tale +=  noun;
 console.log(noun);
 console.log(tale);
-var verb = prompt("Hello again, give me a verb please");
+var verb = prompt("Hello again, give me a verb please" + questionsLeft);
 console.log(verb);
 tale += " that really enjoys to " + verb + ".</h2>";
 document.write(tale);
